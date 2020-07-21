e92004e57dab0a09845e71e8b42dc1ca5f8a3b25
Graeme Clendinning <gclendinning02@gmail.com>
2020-07-21 10:04:25 +0100

    Committing test fix

---
+++ src/test/org/gclendinning02/library/LibraryTest.java
@@ -0,0 +1,12 @@
+package org.gclendinning02.library;
+
+import org.junit.Test;
+import org.squarecell.wow.addon_support.wizard_steps.ui_components.AddOnConfigPanel;
+
+public class LibraryTest
+{
+    @Test
+    public void mockTest() {
+        final AddOnConfigPanel configPanel = new AddOnConfigPanel();
+    }
+}
